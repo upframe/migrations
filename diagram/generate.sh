2afee60d63862c4d6e0c96619bd003d918bc3ef0
Mathis Bullinger <mathis@bullinger.dev>
2020-10-22 19:50:48 +0200

    ci: generate & upload ERD diagram

---
+++ diagram/generate.sh
@@ -0,0 +1,9 @@
+sh $(ls -d diagram/tmp/*/|head -n 1)_schemacrawler/schemacrawler.sh \
+  --command=schema \
+  --output-format=png \
+  --output-file=upframe_latest.png \
+  --info-level=maximum \
+  --server=postgresql \
+  --database=upframe \
+  --user=postgres \
+  --password=postgres
