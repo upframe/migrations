38bc61fc92b85c6acacdafde0b8a382048e6bff7
Mathis Bullinger <mathis@bullinger.dev>
2020-11-09 13:10:20 +0100

    docs: remove foreign key names from diagram

--- diagram/generate.sh
+++ diagram/generate.sh
@@ -1,5 +1,6 @@
 sh $(ls -d diagram/tmp/*/|head -n 1)_schemacrawler/schemacrawler.sh \
   --command=schema \
+  --portable-names=true \
   --output-format=png \
   --output-file=upframe_latest.png \
   --info-level=maximum \
